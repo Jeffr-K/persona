482bf91285ab1816f0ee27212cb386896cb0e921
jeff.cofos <jeff.cofos@gmail.com>
2023-09-03 19:38:58 +0900

    init: sqlalchemy

--- main.py
+++ main.py
@@ -1,13 +1,36 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy import Column, Integer, String, CHAR
 
 app = FastAPI()
 
+SQLALCHEMY_DATABASE_URL = f"postgresql+psycopg2://postgres:root@localhost:5432/persona_db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, echo=True)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
-@app.get("/")
-async def root():
-    return {"message": "Hello World"}
+Base = declarative_base()
 
 
-@app.get("/hello/{name}")
-async def say_hello(name: str):
-    return {"message": f"Hello {name}"}
+def get_db():
+    db = None
+    try:
+        db = SessionLocal()
+        yield db
+    finally:
+        db.close()
+
+
+class Users(Base):
+    __tablename__ = "User"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nickname = Column(String(40))
+    gender = Column(CHAR(2))
+    age = Column(Integer())
+
+
+Base.metadata.create_all(engine)
+
+
